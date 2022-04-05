87522c3572a005096858e0fd55e7ee0c19e6dbe3
j0nSn0w <48774200+RANJUBISWAS@users.noreply.github.com>
2022-04-05 21:47:05 +0530

    Update binary_to_dec.py

--- binary_to_dec.py
+++ binary_to_dec.py
@@ -1,11 +1,13 @@
 num = int(input("Enter Number:"))
-digit = 0
+base = 2 #to convert from any other number system to decimal,
+         #change this variable to the base of the number system
+digit = 0 
 temp = num
 dec = 0
 while(temp):
     digit += 1
     temp //= 10
 for i in range(0 , digit):
-    dec += ((num%10)*(2**i))
+    dec += ((num%10)*(base**i))
     num //=10
 print(dec)
