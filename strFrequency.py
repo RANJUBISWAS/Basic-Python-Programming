8e15f8d9803526b08c545e97a08aff16a0077dcb
j0nSn0w <48774200+RANJUBISWAS@users.noreply.github.com>
2022-06-05 11:17:54 +0530

    updating strFrequency with new procedure.

--- strFrequency.py
+++ strFrequency.py
@@ -6,7 +6,7 @@ print(inputStr)
 uniqueAlphabate = []
 frequency = []
 for alphabate in inputStr:
-    if alphabate != " " and alphabate not in uniqueAlphabate:
+    if alphabate != " " and alphabate not in uniqueAlphabate: #if you want whitespace to be counted,just write -> "if aplhabate not in uniqueAlphabate"
         uniqueAlphabate.append(alphabate)
         frequency.append(1)
     elif alphabate == " ":
@@ -18,3 +18,21 @@ length = len(uniqueAlphabate)
 print("Frequency of each alphabate:")
 for i in range(length):
     print(uniqueAlphabate[i],"=",frequency[i])
+ 
+#----------------------------------------------------------------
+#a better way to do it will be:(following code will ignore all whitespace,tab and new line)
+
+import string
+def strFrequency (inputStr):
+  inputStr = inputStr.translate(str.maketrans('','',string.whitespace))
+  tempDict = {}
+  for alphabate in inputStr:
+    if alphabate not in tempDict:
+      tempDict[alphabate] = 1
+    else: 
+      tempDict[alphabate] += 1
+  return tempDict
+
+testStr = strFrequency("""h   i 
+there!""")
+print(testStr)
